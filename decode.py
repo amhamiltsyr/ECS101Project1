d4ba7748ee2a9100a11b119f4889c1bcf63b35f4
Ashley Hamilton <91693425+amhamiltsyr@users.noreply.github.com>
2021-10-11 18:56:02 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -3,3 +3,13 @@
 # returns D.B and prints
 # D = number of bits needed
 # B = binary representation
+my_file = open("decode_input.txt")
+my_string = my_file.read()
+result = ""
+for i in range(0, len(my_string)):
+    val = findcode(my_string[i])
+    my_ans = my_ans + value
+
+
+def findcode(my_char):
+    if my_char == "a":
