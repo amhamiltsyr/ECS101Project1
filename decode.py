eed7637463e46f1a5218ea239f2a58d11f92f6e3
Ashley Hamilton <91693425+amhamiltsyr@users.noreply.github.com>
2021-10-11 19:00:03 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -6,10 +6,14 @@
 my_file = open("decode_input.txt")
 my_string = my_file.read()
 result = ""
+
+
+def findcode(my_char):
+    pass
+
+
 for i in range(0, len(my_string)):
     val = findcode(my_string[i])
     my_ans = my_ans + value
 
 
-def findcode(my_char):
-    if my_char == "a":
