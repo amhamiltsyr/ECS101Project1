167906284a9cc9c75d071efe63e194f5f63b1e17
almirchowdhury <aachowdh@syr.edu>
2021-10-11 19:26:39 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -11,7 +11,50 @@ bits = 0
 
 def findcode(my_char):
     if my_char == "a":
-        code = "1000"
+        code = "00000"
+    elif my_char == "e":
+        code = "00001"
+    elif my_char == "i":
+        code = "00010"
+    elif my_char == "o":
+        code = "00011"
+    elif my_char == "u":
+        code = "00100"
+    elif my_char == ".":
+        code = "00101"
+    elif my_char == ",":
+        code = "00110"
+    elif my_char == "A":
+        code = "00111"
+    elif my_char == "E":
+        code = "01000"
+    elif my_char == "I":
+        code = "01001"
+    elif my_char == "O":
+        code = "01010"
+    elif my_char == " ":
+        code = "01011"
+    elif my_char == "t":
+        code = "01100"
+    elif my_char == "s":
+        code = "01101"
+    elif my_char == "r":
+        code = "01110"
+    elif my_char == "T":
+        code = "01111"
+    elif my_char == "-":
+        code = "1100110"
+    elif my_char == "!":
+        code = "1100111"
+    elif my_char == "'":
+        code = "1101000"
+    elif my_char == "”":
+        code = "1101001"
+    elif my_char == "\n":
+        code = "1101010"
+    elif my_char == "U":
+        code = "1101011"
+
     return code
 
 
